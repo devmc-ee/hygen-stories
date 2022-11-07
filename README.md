558050834938deaadd9521c7dbf22327d4da50ee
Devmcee <info@devmc.ee>
2022-11-07 23:36:27 +0200

    fix folders parsing

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # hygen-stories
-CLI wrapper for hygen to generate stories for existing components (only React at the moment)
+CLI wrapper for [hygen](https://www.hygen.io/) to generate [storybook](https://storybook.js.org/) stories for the existing components (only React at the moment, .tsx)
+
+It recursivly scans the given folder. If it contains .tsx file and no a .stories.tsx, then it generates one. Name of the file and the name of the components that will be used in the story is taken from the .tsx file name. New name will be in the Pascal Case. Hyphen will be replaced and every part of the name be capetilised: 
+
+>component-name.tsx  => ComponentName.tsx
+
+
+## Install
+
+```
+npm i @devmcee/hygen-stories -D
+```
+
+Usage example:
+```javascript
+const { folderStructureMap } = require('@devmcee/folder-content-map');
+
+const structure = await folderStructureMap('src/components', ['.tsx', '.scss']);
+
+console.log(structure)
+
+```
+
+Sample data output:
+```
+[
+  {
+    type: 'directory',
+    parent: './src',
+    path: '/home/devmcee/dev/folder-content-map/src/components',
+    name: 'components',
+    innerItems: [
+      {
+        type: 'file',
+        parent: '/home/devmcee/dev/folder-content-map/src/components',
+        itemPath: '/home/devmcee/dev/folder-content-map/src/components/index.js',
+        name: 'index.js',
+        innerItems: []
+      }
+    ]
+  }
+]
+```
+
+There is an executable script that can be called with npx:
+
+```bash
+npx folder-content-map --dir src 
+```
+
+```bash
+Options:
+      --help     Show help                                             [boolean]
+      --version  Show version number                                   [boolean]
+  -d, --dir      directory name, relative path, default is current (./)
+
+Examples:
+  generate.js --dir src/components 
+```
+
+### TODO:
+Use AST analysis of  .tsx files to grap the real name of the defined and exported components. 
---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const ITEM_TYPE = {
+  file: 'file',
+  directory: 'directory',
+};
+
+function getFileBaseName(fileName, extension) {
+  return fileName.slice(0, fileName.length - extension.length);
+}
+
+function getComponentName(fileName, extension) {
+  const baseFileName = getFileBaseName(fileName, extension);
+
+  return baseFileName
+    .split('-')
+    .map((part) => `${part.slice(0, 1).toUpperCase()}${part.slice(1)}`)
+    .join('');
+}
+
+/**
+ *
+ * @param {{
+ *  type: string;
+ *  parent: string;
+ *  itemPath: string;
+ *  name: string;
+ *  innerItems: {
+ *      type: string;
+ *      parent: string;
+ *      itemPath: string;
+ *      name: string;
+ *      innerItems: {}[]
+ *  }[]}[]} items
+ */
+async function generateStories(items) {
+  let result = true;
+
+  const hasStory = items.some((item) => item.name.endsWith('.stories.tsx'));
+
+  if (hasStory) {
+    console.log('skip folder: ', items[0].parent);
+    return false;
+  }
+
+  const componentFile = items.find((item) => item.name.endsWith('.tsx'));
+  
+  if (componentFile) {
+    const { name, parent } = componentFile;
+
+    const componentName = getComponentName(name, '.tsx');
+    const fileBaseName = getFileBaseName(name, '.tsx');
+
+    const stylesFile = items.find((item) => item.name.endsWith('.scss'));
+
+    const stylesNameOption = stylesFile ? `${stylesFile.name}` : 'null';
+
+    try {
+      const command = `npx hygen story new --name ${componentName} --path ${parent} --fileName ${fileBaseName} --styles ${stylesNameOption}`;
+      
+      await exec(command);
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log(componentName);
+  }
+
+  const directories = items.filter((item) => item.type === ITEM_TYPE.directory);
+
+  for (const directory of directories) {
+    const { innerItems } = directory;
+
+    if (innerItems.length) {
+      const result = await generateStories(innerItems);
+
+      if (!result) continue;
+    }
+  }
+
+  return result;
+}
+
+module.exports = { generateStories };
